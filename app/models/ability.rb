4889dacff66bfe8599b973a2803d381f20e6ca37
arvoriz <ian.alvares0520@gmail.com>
2026-03-01 12:06:05 -0300

    relacionando permissão de venda com itens da venda

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -7,6 +7,11 @@ class Ability
     funcionario.cargo.permissoes.each do |permissao|
       alvo = permissao.tabela == 'all' ? :all : permissao.tabela.constantize
       can permissao.acao.to_sym, alvo
+
+      # Se a pessoa pode fazer algo com Venda, ela também pode fazer com os itens daquela venda
+      if alvo == Venda || alvo == :all
+        can permissao.acao.to_sym, ItensDaVenda
+      end
     end
   end
 end
