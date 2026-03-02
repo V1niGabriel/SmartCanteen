25cbf4ba4a305e6c95ad24c85a0d16c4a5555877
Vinicius Gabriel <vinigabi256545@gmail.com>
2026-03-02 16:25:24 -0300

    Configuração do arquivo de log e pequenas alterações no .css

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -4,5 +4,7 @@
 # Use this to limit dissemination of sensitive information.
 # See the ActiveSupport::ParameterFilter documentation for supported notations and behaviors.
 Rails.application.config.filter_parameters += [
-  :passw, :email, :secret, :token, :_key, :crypt, :salt, :certificate, :otp, :ssn, :cvv, :cvc
+  :passw, :email, :secret, :token, :_key, :crypt, :salt, :certificate, :otp, :ssn, :cpf, :cvv, :cvc
 ]
+
+# Essa filtro impede que sejam salvos dados sensíveis com cpf e senha no log
